372d6e8ab243987ed041d1c9355d7c6e822db64b
dom64 <20028190+dom64@users.noreply.github.com>
2024-03-09 01:06:09 -0500

    Improved the jad gangstalking (added file mirroring)

--- cogs/jadmirror.py
+++ cogs/jadmirror.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from io import BytesIO
 
 class JadMirror(commands.Cog):
     def __init__(self, bot):
@@ -9,7 +10,14 @@ class JadMirror(commands.Cog):
     async def on_message(self, message):
         if message.author.id == 176322126129790976:
             channel = self.bot.get_channel(1093261714931322901)
-            await channel.send(f'{message.author.display_name}: {message.content}')
+            if message.content:
+                await channel.send(f'{message.author.display_name}: {message.content}')
+            if message.attachments:
+                for attachment in message.attachments:
+                    file = await attachment.read()
+                    mirror = discord.File(BytesIO(file), filename=attachment.filename)
+                    await channel.send(f'{message.author.display_name}: {attachment.url}', file=mirror)
+        await self.bot.process_commands(message)
 
 async def setup(bot):
     await bot.add_cog(JadMirror(bot))
