cd67d8ebb6b2017cf37903bc1756f5cf2e2352ab
dom64 <20028190+dom64@users.noreply.github.com>
2024-04-15 21:03:16 -0400

    Loser patch v2

--- cogs/economy.py
+++ cogs/economy.py
@@ -82,7 +82,7 @@ class Economy(commands.Cog):
         print(top_ten)
         format = "Top 10:\n"
         for x in top_ten:
-            format += f"@silent <@{x[0]}> has ${x[1]}\n"
+            format += f"<@{x[0]}> has ${x[1]}\n"
         await ctx.send(format, silent=True)
 
     @commands.command()
