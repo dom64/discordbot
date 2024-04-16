4918e545d613b528c7f3c31d05796349ae7b04b4
dom64 <20028190+dom64@users.noreply.github.com>
2024-04-16 17:48:49 -0400

    Added msg command

--- cogs/owner.py
+++ cogs/owner.py
@@ -14,5 +14,18 @@ class Owner(commands.Cog):
         await self.bot.tree.sync()
         await ctx.send("Synced slash commands")
 
+    @commands.is_owner()
+    @commands.command()
+    async def sendmsg(self, ctx, user: discord.User, *, msg):
+        await user.send(msg)
+        await ctx.send("Message sent!")
+    
+    @sendmsg.error
+    async def sendmsg_error(self, ctx, error):
+        if isinstance(error, discord.ext.commands.errors.UserNotFound):
+            await ctx.send("Error: Member not found")
+        else:
+            raise error
+
 async def setup(bot):
     await bot.add_cog(Owner(bot))
